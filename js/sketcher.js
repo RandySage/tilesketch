0f0d19dfb9f8c05dd0eb6eb2b6bb9f303b6dec43
R.Sage <rsage@thinhorn.com>
2012-10-27 22:20:31 -0600

    initial crude support for grayscale as well as color stub

--- js/sketcher.js
+++ js/sketcher.js
@@ -1,6 +1,28 @@
-function Sketcher( canvasID, brushImage ) {
-	this.renderFunction = (brushImage == null || brushImage == undefined) ? this.updateCanvasByLine : this.updateCanvasByBrush;
-	this.brush = brushImage;
+function Sketcher( canvasID, colorType ) {
+    this.renderFunction =  this.updateCanvasByLine;
+    this.nTiles = 16;
+
+    // Used only for color0
+    this.redMult = 1;
+    this.blueMult = 1;
+    this.greenMult = 1;
+    // end color0
+    
+    if (colorType == 'blackWhite' || colorType == undefined){
+	this.colorType = 'blackWhite';
+    } 
+    else if (colorType == 'grayScale') {
+	this.colorType = 'grayScale';
+	this.nTiles = 160;
+    } 
+    else if (colorType == 'color0') {
+	this.colorType = 'color0';
+	this.nTiles = 160;
+    }
+    else{
+	// Default to blackWhite
+	this.colorType = 'blackWhite';
+    }
 	this.touchSupported = Modernizr.touch;
 	this.canvasID = canvasID;
 	this.canvas = $("#"+canvasID);
@@ -13,7 +35,6 @@ function Sketcher( canvasID, brushImage ) {
 	this.lastMousePoint = {x:0, y:0};
 	this.prevLastMousePoint = {x:0, y:0};
 	
-	this.nTiles = 16;
     
 	if (this.touchSupported) {
 		this.mouseDownEvent = "touchstart";
@@ -84,13 +105,44 @@ Sketcher.prototype.updateMousePosition = function (event) {
 
 }
 
+Sketcher.prototype.getColor0 = function ( i, m) {
+    var frequency = 2*Math.PI / m;
+    var offset = 128;
+    var amplitude = 127;
+
+    var redOffset = 0;
+    var blueOffset = 3;
+    var greenOffset = 5;
+
+    var red   = Math.round( Math.sin(this.redMult*frequency*i + redOffset) * amplitude + offset );
+    var green = Math.round( Math.sin(this.greenMult*frequency*i + greenOffset) * amplitude + offset );
+    var blue  = Math.round( Math.sin(this.blueMult*frequency*i + blueOffset) * amplitude + offset );
+	    
+    var colorString = "rgb(" + red + "," + green + "," + blue + ")";
+    return colorString;
+}
+
 Sketcher.prototype.updateCanvasByLine = function (event) {
 	this.context.moveTo( this.lastMousePoint.x, this.lastMousePoint.y );
 	this.updateMousePosition( event );
 	var tileAngle = 2*Math.PI / this.nTiles;
 	var xCent = this.canvas.width() / 2;
 	var yCent = this.canvas.height() / 2;
+	var strokeStyle;
 	for (var i = 0; (i < this.nTiles); i++){
+	    if (this.colorType == 'blackWhite'){
+		strokeStyle = "rgb(0, 0, 0)";
+	    } 
+	    else if (this.colorType == 'grayScale') {
+		var colorVal = 255 - ((15*i) % 256);
+		this.context.strokeStyle = "rgb(" + colorVal + "," + colorVal + "," + colorVal + ")"
+	    } 
+	    else if (this.colorType == 'color0') {
+		this.colorFreq = 1.5 * Math.sqrt(this.nTiles);
+
+		strokeStyle = this.getColor0(i,this.colorFreq);
+	    }
+
 		var angle = i*tileAngle;
 		var x0 = Math.cos(angle)*(this.prevLastMousePoint.x-xCent) + 
 			-Math.sin(angle)*(this.prevLastMousePoint.y-yCent);
@@ -103,33 +155,12 @@ Sketcher.prototype.updateCanvasByLine = function (event) {
 		this.context.beginPath();
 		this.context.moveTo( x0+xCent, y0+yCent );
 		this.context.lineTo( x1+xCent, y1+yCent );
+		this.context.strokeStyle = strokeStyle;
 		this.context.stroke();
 	}
 
 	this.updateMousePosition( event );
 //	this.context.moveTo( this.lastMousePoint.x, this.lastMousePoint.y );
-}
-
-Sketcher.prototype.updateCanvasByBrush = function (event) {
-	var halfBrushW = this.brush.width/2;
-	var halfBrushH = this.brush.height/2;
-	
-	var start = { x:this.lastMousePoint.x, y: this.lastMousePoint.y };
-	this.updateMousePosition( event );
-	var end = { x:this.lastMousePoint.x, y: this.lastMousePoint.y };
-	
-	var distance = parseInt( Trig.distanceBetween2Points( start, end ) );
-	var angle = Trig.angleBetween2Points( start, end );
-	
-	var x,y;
-	
-	for ( var z=0; (z<=distance || z==0); z++ )
-	{
-		x = start.x + (Math.sin(angle) * z) - halfBrushW;
-		y = start.y + (Math.cos(angle) * z) - halfBrushH;
-		//console.log( x, y, angle, z );
-		this.context.drawImage(this.brush, x, y);
- 	}
 }
 
 Sketcher.prototype.toString = function () {
